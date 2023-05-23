683b0e60e12711f5f1a1cb54b6a54750fd2eb349
Ryan-Holmes-363 <96438279+Ryan-Holmes-363@users.noreply.github.com>
2023-05-23 10:42:32 -0500

    Add files via upload

---
+++ create_data_lists.py
@@ -0,0 +1,6 @@
+from utils import create_data_lists
+
+if __name__ == '__main__':
+    create_data_lists(voc07_path='C:/Users/iused/OneDrive/Desktop/CVStuff/a-PyTorch-Tutorial-to-Object-Detection-master/VOCsets/merge2007/VOCdevkit/VOC2007',
+                      voc12_path='C:/Users/iused/OneDrive/Desktop/CVStuff/a-PyTorch-Tutorial-to-Object-Detection-master/VOCsets/train2012/VOCdevkit/VOC2012',
+                      output_folder='C:/Users/iused/OneDrive/Desktop/CVStuff/a-PyTorch-Tutorial-to-Object-Detection-master/create_data_lists_output')
